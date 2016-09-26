0f16360064517f7d15adaabfecd896c4451c15a9
just-paja <just.paja@gmail.com>
2016-09-26 17:34:46 +0200

    Remove unused ngrok

--- src/server/main.js
+++ src/server/main.js
@@ -14,7 +14,6 @@ import { resolve } from 'path';
 
 const argv = require('minimist')(process.argv.slice(2));
 const isDev = process.env.NODE_ENV !== 'production';
-const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
 
 const app = express();
 const db = databaseInit();
@@ -52,17 +51,6 @@ db.connect()
         return logger.error(err.message);
       }
 
-      // Connect to ngrok in dev mode
-      if (ngrok) {
-        ngrok.connect(port, (innerErr, url) => {
-          if (innerErr) {
-            return logger.error(innerErr);
-          }
-
-          logger.appStarted(port, url);
-        });
-      } else {
-        logger.appStarted(port);
-      }
+      logger.appStarted(port);
     });
   });
