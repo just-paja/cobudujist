cca5bd137c291820f49efe290b2c0dafd1b4e97f
just-paja <just.paja@gmail.com>
2016-09-26 15:28:55 +0200

    Drop lodash because it's crap

--- internals/config.js
+++ internals/config.js
@@ -1,6 +1,4 @@
 const resolve = require('path').resolve;
-const pullAll = require('lodash/pullAll');
-const uniq = require('lodash/uniq');
 
 const ReactBoilerplate = {
   // This refers to the react-boilerplate version this project is based on.
@@ -20,27 +18,13 @@ const ReactBoilerplate = {
        * we need to exclude dependencies which are not intended for the browser
        * by listing them here.
        */
-      exclude: [
-        'chalk',
-        'compression',
-        'cross-env',
-        'express',
-        'ip',
-        'minimist',
-        'sanitize.css',
-        'sqlite3',
-        'mysql',
-        'express-device',
-        'serialize-javascript',
-        'sequelize',
-        'css-modules-require-hook',
-      ],
+      exclude: [],
 
       /**
-       * Specify any additional dependencies here. We include core-js and lodash
+       * Specify any additional dependencies here. We include core-js
        * since a lot of our dependencies depend on them and they get picked up by webpack.
        */
-      include: ['core-js', 'eventsource-polyfill', 'babel-polyfill', 'lodash'],
+      include: ['core-js', 'eventsource-polyfill', 'babel-polyfill'],
 
       // The path where the DLL manifest and bundle will get built
       path: resolve('../node_modules/react-boilerplate-dlls'),
@@ -50,10 +34,15 @@ const ReactBoilerplate = {
       const dependencyNames = Object.keys(pkg.dependencies);
       const exclude = pkg.dllPlugin.exclude || ReactBoilerplate.dllPlugin.defaults.exclude;
       const include = pkg.dllPlugin.include || ReactBoilerplate.dllPlugin.defaults.include;
-      const includeDependencies = uniq(dependencyNames.concat(include));
+      const includeDependencies = dependencyNames
+        .concat(include)
+        .filter((value, index, self) => self.indexOf(value) === index)
+        .filter(value => exclude.indexOf(value) === -1);
+
+      console.log(includeDependencies);
 
       return {
-        reactBoilerplateDeps: pullAll(includeDependencies, exclude),
+        reactBoilerplateDeps: includeDependencies,
       };
     },
   },
